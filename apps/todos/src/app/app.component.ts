92a2946749b02ce59320d10524b12040cf839ec2
Anton Todorov <atodor@softserveinc.com>
2020-12-09 20:03:57 +0200

    create ui library

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface Todo {
-  title: string;
-}
+import { Todo } from '@myorg/data';
 
 @Component({
   selector: 'myorg-root',
