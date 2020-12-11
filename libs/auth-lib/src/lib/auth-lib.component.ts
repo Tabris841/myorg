03c64f75157ea9e5f9b9bb2d3facc84f455fb96c
Anton Todorov <shadowtabris84@gmail.com>
2020-12-11 16:17:33 +0200

    chore: new build

--- libs/auth-lib/src/lib/auth-lib.component.ts
+++ libs/auth-lib/src/lib/auth-lib.component.ts
@@ -3,7 +3,7 @@ import { AuthLibService } from './auth-lib.service';
 
 @Component({
   selector: 'myorg-auth-lib',
-  template: ` <p>Hi User: {{ user }}!!@</p> `,
+  template: ` <p>Hi User: {{ user }}!!@@@</p> `,
 })
 export class AuthLibComponent {
   user = this.service.user;
